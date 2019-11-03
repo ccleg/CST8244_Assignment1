2bd99c3e6141d87bdb883bd5570ccfec739d9cd6
MasterVick1 <47720444+MasterVick1@users.noreply.github.com>
2019-11-03 00:40:50 -0400

    Update des_inputs.c
    changed a few things around

--- des_inputs/src/des_inputs.c
+++ des_inputs/src/des_inputs.c
@@ -5,9 +5,12 @@
 #include <sys/netmgr.h>
 #include <string.h>
 #include <errno.h>
+#include "../../des_controller/src/ASS1.h"
 
 int main(int argc, char *argv[]) {
-	char *input_msg;
+
+	Person_T person;
+	server_response_t response;
 
 	char* rplyMsg;
 	if (argc != 2){
@@ -27,12 +30,27 @@ int main(int argc, char *argv[]) {
 		perror("Channel could not be attached\n");
 		exit(EXIT_FAILURE);
 	}
-	while(strcmp(input_msg,"exit") != 0){
-		printf(":\n");
-		input_msg = "glu";
-		if(MsgSend(coid, input_msg, strlen(input_msg) + 1, rplyMsg, sizeof (rplyMsg)) == -1){
-			fprintf(stderr, "Unable to send message\n");
-			//perror(NULL);
+	while(person.currentState!= 10){
+		printf("Enter the event type (ls = left scan, rs = right scan, ws = weight scan, lo = left open, \n"
+				"ro = right open, lc = left close, rc = rights closed, gru = guard right unlock \n"
+				"glu = guard left unlock, grl = guard right lock, gll = guard left lock\n");
+		printf("Enter 'exit' to quit the program.\n");
+
+		scanf("&s", person.currentState);
+
+		if(person.currentState == 0){
+			printf("Enter your ID:\n");
+			scanf("%d", person.ID);
+		}
+
+		else if(person.currentState == 3){
+			printf("Enter your weight:\n");
+			scanf("%d", person.Weight);
+		}
+
+		if(MsgSend(coid, &person, sizeof(person), &response, sizeof(response)) == -1){
+			fprintf(stderr, "Error during MsgSend\n");
+			perror(NULL);
 			exit(EXIT_FAILURE);
 		}
 
